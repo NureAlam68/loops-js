58ec71597cfab0018ff2e81252fa7e78662b24ef
NureAlam68 <nurealam151068@gmail.com>
2024-09-01 19:26:46 +0600

    added odd loop

--- range.js
+++ range.js
@@ -1,8 +1,31 @@
 // 1 to 10 number loop
 
+// let num = 1;
+
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// }
+
+
+
+
+// 50 to 100 number
+
+
+// let num = 50;
+
+// while(num <= 100){
+//     console.log(num);
+//     num++;
+// }
+
+
+// odd number
+
 let num = 1;
 
-while(num <= 10){
+while(num < 20){
     console.log(num);
-    num++;
+    num = num + 2;
 }
