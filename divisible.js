bca3c41a72e6acc7a40c5567959dca1954966881
NureAlam68 <nurealam151068@gmail.com>
2024-09-01 22:45:59 +0600

    divisible js

---
+++ divisible.js
@@ -0,0 +1,44 @@
+// 1 to 30 divisible 3 or 5
+
+
+
+// divisible by 3
+
+// for(let i = 1; i <= 30; i++){
+//     if(i % 3 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// divisible by 5
+
+// for(let i = 1; i <= 30; i++){
+//     if(i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// divisible by 3 or 5
+
+// for(let i = 1; i <= 30; i++){
+//     if(i % 3 === 0 || i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// divisible by 3 and 5
+
+for(let i = 1; i <= 30; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(i);
+    }
+}
