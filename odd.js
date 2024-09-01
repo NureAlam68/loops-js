5826e3b464ac2b6e8b586d02f4db9ff2ad57f70b
NureAlam68 <nurealam151068@gmail.com>
2024-09-01 22:31:33 +0600

    odd number

---
+++ odd.js
@@ -0,0 +1,20 @@
+// for loop 
+
+// for(let i=1; i <= 20; i+=2){
+//     console.log(i);
+// }
+
+
+// for(let i=1; i <= 20; i++){
+//     if(i % 2 === 1){
+//         console.log(i);
+//     }
+// }
+
+
+
+for(let i=1; i <= 20; i++){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
