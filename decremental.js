99ee25bb7a682a1cdad01dd3a4cd6f7453ab12c6
NureAlam68 <nurealam151068@gmail.com>
2024-09-01 21:57:04 +0600

    decremental js

---
+++ decremental.js
@@ -0,0 +1,21 @@
+// use for loop
+
+
+
+// 20 to 10
+
+// for(let i = 20; i >= 10; i--){
+//     console.log(i);
+// }
+
+
+
+// use while loop
+
+
+let i = 20;
+
+while(i >= 10){
+    console.log(i);
+    i--;
+}
