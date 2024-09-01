c2bd5827ccda03efa6a54a877c6bae8cf5f50d7c
NureAlam68 <nurealam151068@gmail.com>
2024-09-01 19:02:20 +0600

    added loops javascript

---
+++ for-of.js
@@ -0,0 +1,32 @@
+// for of loop
+
+
+
+
+// number array
+
+// let numbers = [12, 13, 1, 2, 3, 4, 5, 6, 7];
+
+// console.log(numbers);
+
+// for(let num of numbers) {
+//     console.log(num);
+//     console.log("Love you");
+// }
+
+
+
+
+
+// string array
+
+let iToldYou = ["I love You", "I love You", "I love You"];
+
+console.log(iToldYou);
+
+for(let told of iToldYou) {
+    console.log(told);
+    console.log("Jui");
+}
+
+
