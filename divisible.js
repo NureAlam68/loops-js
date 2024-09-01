2a90743d929cc1fc514c4e5417af2c95de28f32a
NureAlam68 <nurealam151068@gmail.com>
2024-09-01 22:53:38 +0600

    added sum with divisible

--- divisible.js
+++ divisible.js
@@ -37,8 +37,25 @@
 
 // divisible by 3 and 5
 
-for(let i = 1; i <= 30; i++){
-    if(i % 3 === 0 && i % 5 === 0){
+// for(let i = 1; i <= 30; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// give sum between 1 to 20 which is divisible by 3
+
+let sum = 0;
+
+for(let i = 1; i <= 20; i++){
+    if(i % 3 === 0){
         console.log(i);
+        sum = sum + i;
+        console.log('sum', sum)
     }
 }
+
+console.log('sum of the numbers', sum);
